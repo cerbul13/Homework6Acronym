5a2e1de87aef79f22b1c277887d0a43316263a6b
cerbul13 <cerbadan@yahoo.com>
2022-01-06 15:07:02 +0200

    Add project files.

---
+++ Homework6Acronym/Acronym.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework6Acronym
+{
+    public class Acronym
+    {
+        private string text;
+        private string theAcronym;
+        public string Text
+        {
+            get { return text; }
+        }
+        public string TheAcronym
+        {
+            get {
+                string acro = "";
+                bool nextAcro = true;
+                for (int i = 0; i < text.Length; i++) 
+                {
+                    if (nextAcro==true && text[i]!=' ')
+                    {
+                        acro += text[i].ToString().ToUpper();
+                        nextAcro = false;
+                    }
+                    if (text[i] == ' ') nextAcro = true;
+                }
+                theAcronym = acro;
+                return acro;
+                }
+        }
+        public Acronym(string text)
+        {
+            this.text = text;
+        }
+    }
+}
---
+++ Homework6Acronym/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Homework6Acronym
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Acronym acronym = new Acronym("The Best idea");
+            Console.WriteLine(acronym.TheAcronym);
+            Acronym acronym2 = new Acronym("Portable Network Graphics");
+            Console.WriteLine(acronym2.TheAcronym);
+        }
+    }
+}
